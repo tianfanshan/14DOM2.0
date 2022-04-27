8f69335591009571589c3c5452b7f11eb7374651
laopengyou <stf30111994@gmail.com>
2022-04-27 08:27:47 +0200

    14DOM

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 console.log(document.title);
+
+document.getElementById("gen-1").innerText = "Generasión 1 Pokimon";
+
+// document.getElementById("backgound1").style.background = "red";??
+//document.getElementsByClassName("infocard-list infocard-list-pkmn-lg").style.backgound = "red";??
+
+let url = document.URL;
+console.log(url);
+
+let url01 = document.location;
+console.log(url01);
+
+let nodosDeImagen = document.getElementsByClassName("infocard-lg-img");
+console.log(nodosDeImagen);
+
+for(let i = 0 ; i < nodosDeImagen.length ; i++){
+    console.log(nodosDeImagen[i])
+    nodosDeImagen.innerText = ""
+}
