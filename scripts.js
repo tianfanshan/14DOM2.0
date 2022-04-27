4ba902aa6705369776c3c2cb6fe94072aaaa3880
tianfanshan <stf30111994@gmail.com>
2022-04-27 11:24:06 +0200

    14DOM

--- scripts.js
+++ scripts.js
@@ -2,8 +2,7 @@ console.log(document.title);
 
 document.getElementById("gen-1").innerText = "Generasión 1 Pokimon";
 
-// document.getElementById("backgound1").style.background = "red";??
-//document.getElementsByClassName("infocard-list infocard-list-pkmn-lg").style.backgound = "red";??
+document.getElementById("gen-1").nextElementSibling.style.background = "red";
 
 let url = document.URL;
 console.log(url);
@@ -16,5 +15,11 @@ console.log(nodosDeImagen);
 
 for(let i = 0 ; i < nodosDeImagen.length ; i++){
     console.log(nodosDeImagen[i])
-    nodosDeImagen[i].src = "./asset/undefined - Imgur.gif"
+    nodosDeImagen[i].src = "https://media2.giphy.com/media/2v170e71aanfi/giphy.gif?cid=790b76110f98452404c016ea19c7c5ec5d84ae03195eccbd&rid=giphy.gif&ct=s"
+}
+
+let varios = document.getElementsByClassName("itype flying");
+for(let i = 0 ; i < varios.length ; i++){
+    console.log(varios[i])
+    varios[i].parentNode.parentNode.style.background = "green";
 }
